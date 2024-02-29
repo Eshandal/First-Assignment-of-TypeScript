3000ce3153cb5871c3a95cc7d329400c8e407735
Eshan Dal <159187977+Eshandal@users.noreply.github.com>
2024-02-29 13:44:38 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// String
+var personName = "ESHAN";
+console.log(personName);
+// Number 
+var age = 18;
+console.log(age);
+// Boolean 
+var isstudent = true;
+console.log(isstudent);
+// Null
+var empty = null;
+console.log(empty);
+// Undefined
+var divide = undefined;
+console.log(divide);
+// Bigint 
+var bigNumber = 839104422040870;
+console.log(bigNumber);
---
+++ index.ts
@@ -0,0 +1,25 @@
+// String
+let personName : string ="ESHAN";
+console.log(personName);
+
+// Number 
+let age : number = 18;
+console.log(age);
+
+// Boolean 
+let isstudent : boolean = true;
+console.log(isstudent);
+
+// Null
+let empty = null ;
+console.log(empty);
+
+// Undefined
+let divide = undefined;
+console.log(divide);
+
+// Bigint 
+let bigNumber =839104422040870;
+console.log(bigNumber);
+
+
